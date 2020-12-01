2d316356caf5fa93b2f9234ab2f57031f36543d8
maxlath <k@maxlath.eu>
2020-12-01 21:47:21 +0100

    clear_language_terms: convert to wikibase-cli v15.7.0 new recommended template format

--- edit/clear_language_terms.js
+++ edit/clear_language_terms.js
@@ -1,23 +1,28 @@
 // Inspired by https://github.com/generalist/wikidata-misc/blob/e7688cc/labelcleaner.sh
+// Requires wikibase-cli >= v15.6.0
 
-module.exports = function (id, lang) {
-  return {
-    id,
-    labels: {
-      [lang]: null
-    },
-    descriptions: {
-      [lang]: null
-    },
-    aliases: {
-      [lang]: null
-    },
-    summary: `clear ${lang} terms`,
+module.exports = {
+  args: [ '<id>', '<lang>' ],
+
+  description: 'Clear labels, descriptions, and aliases in a given language',
+
+  examples: [
+    { args: [ 'Q4115189', 'fr' ], comment: 'Clear Q4115189 French labels, descriptions, and aliases' },
+  ],
+
+  template: function (id, lang) {
+    return {
+      id,
+      labels: {
+        [lang]: null
+      },
+      descriptions: {
+        [lang]: null
+      },
+      aliases: {
+        [lang]: null
+      },
+      summary: `clear ${lang} terms`,
+    }
   }
 }
-
-module.exports.args = [ '<id>', '<lang>' ]
-module.exports.description = 'Clear labels, descriptions, and aliases in a given language'
-module.exports.examples = [
-  { args: [ 'Q4115189', 'fr' ], comment: 'Clear Q4115189 French labels, descriptions, and aliases' },
-]
