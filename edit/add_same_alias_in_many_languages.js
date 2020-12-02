6f10ac1241d4eb1ac44b71168d21f15f56bf938a
maxlath <k@maxlath.eu>
2020-12-02 13:23:39 +0100

    + edit/add_same_alias_in_many_languages.js

---
+++ edit/add_same_alias_in_many_languages.js
@@ -0,0 +1,28 @@
+// Requires wikibase-cli >= v15.8.2
+
+// Customize to fit your needs
+const languages = [ 'en', 'de', 'fr', 'it', 'es' ]
+
+module.exports = {
+  args: '<id> <alias>',
+
+  description: `Add the same alias in many languages at once: ${languages.join(', ')}\nEdit this template to customize language list`,
+
+  examples: [
+    { args: [ 'Q4115189', '"sandbox box box box"' ], comment: `Add 'sandbox box box box' to Q4115189 aliases in ${languages.join(', ')}` },
+  ],
+
+  template: function (id, alias) {
+    const editObj = {
+      id,
+      aliases: {},
+      summary: `Add '${alias}' to aliases in ${languages.join(', ')}`,
+    }
+
+    for (const lang of languages) {
+      editObj.aliases[lang] = { value: alias, add: true }
+    }
+
+    return editObj
+  }
+}
