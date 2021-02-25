29edb3c00c44034d73b7c1d7f2c4045cab021326
maxlath <k@maxlath.eu>
2021-02-25 13:57:57 +0100

    + edit/add_parts_bidirectionnal_claims.js

---
+++ edit/add_parts_bidirectionnal_claims.js
@@ -0,0 +1,26 @@
+// Requires wikibase-cli >= v15.10.0
+
+const { isItemId } = require('wikibase-sdk')
+
+module.exports = {
+  args: '<hasPartItem> <partOfItem>',
+
+  description: "Add 'has part' (P527) / 'part of' (P361) claims to a pair of items",
+
+  examples: [
+    {
+      args: [ 'Q1', 'Q3' ],
+      comment: 'Create 2 claims: Q1-P527->Q3 and Q3-P361->Q1'
+    },
+  ],
+
+  template: function (hasPartItem, partOfItem) {
+    if (!isItemId(hasPartItem)) throw new Error(`invalid item id: ${hasPartItem}`)
+    if (!isItemId(partOfItem)) throw new Error(`invalid item id: ${partOfItem}`)
+
+    return [
+      { id: hasPartItem, claims: { P527: partOfItem } },
+      { id: partOfItem, claims: { P361: hasPartItem } },
+    ]
+  }
+}
