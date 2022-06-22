8c88a84e5a77a6f7862e54e164a54fecd97c922b
maxlath <k@maxlath.eu>
2022-06-22 13:54:08 +0200

    edit: add_statement_if_missing: update

--- edit/add_statement_if_missing.js
+++ edit/add_statement_if_missing.js
@@ -1,9 +1,8 @@
-const { getEntity } = require('../lib/helpers')
-const { simplify } = require('wikibase-sdk')
+// Requires wikibase-cli >= v15.16.0
 
 module.exports = {
   commands: [
-    'edit-entity'
+    'add-claim'
   ],
 
   args: '<entity id> <property id> <value>',
@@ -15,29 +14,13 @@ module.exports = {
   ],
 
   template: async (id, property, value) => {
-    const existingClaim = await findExistingClaim(id, property, value)
-    if (existingClaim != null) {
-      console.warn('a statement with that value already exist:', existingClaim.id)
-      // Return an empty list of edits
-      return []
-    } else {
-      // Return a single edit object, with the desired claim value
-      return {
-        id,
-        claims: {
-          [property]: value
-        }
-      }
+    return {
+      id,
+      reconciliation: {
+        mode: 'skip-on-value-match'
+      },
+      property,
+      value,
     }
   }
-}
-
-const findExistingClaim = async (id, property, value) => {
-  const entity = await getEntity(id)
-  const propertyClaims = entity.claims[property]
-  if (!propertyClaims) return
-  const existingClaim = propertyClaims.find(claim => {
-    return simplify.claim(claim) === value
-  })
-  return existingClaim
 }
